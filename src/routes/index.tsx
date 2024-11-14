99c64a9ce83f0eea3322bdfa63f3372d9d45af42
AJimenez0209 <jimenezaaron5@gmail.com>
2024-11-14 18:24:32 -0300

    Fix: Añadir CartProvider al Nivel Superior de la Aplicación

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,18 +2,31 @@ import { createBrowserRouter } from 'react-router-dom';
 import App from '../App';
 import { MallPaymentResult } from '../components/MallPaymentResult';
 import WebpayRedirectPage from '../components/WebpayRedirectPage';
+import { CartProvider } from '../context/CartContext'; // Importa el CartProvider
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />,
+    element: (
+      <CartProvider>
+        <App />
+      </CartProvider>
+    ),
   },
   {
     path: '/payment/result',
-    element: <MallPaymentResult />,
+    element: (
+      <CartProvider>
+        <MallPaymentResult />
+      </CartProvider>
+    ),
   },
   {
     path: '/webpay-redirect',
-    element: <WebpayRedirectPage />,
+    element: (
+      <CartProvider>
+        <WebpayRedirectPage />
+      </CartProvider>
+    ),
   },
 ]);
