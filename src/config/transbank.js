e322e29a3dced6ce634f571dc0088c2c14445c6e
AJimenez0209 <jimenezaaron5@gmail.com>
2024-11-14 21:15:36 -0300

    FIX: Configuración de Transbank para Webpay Plus en una sola tienda

--- src/config/transbank.js
+++ src/config/transbank.js
@@ -1,24 +1,13 @@
-const { Options, Environment, IntegrationCommerceCodes, IntegrationApiKeys } = require('transbank-sdk');
+const { Options, Environment, IntegrationCommerceCodes, IntegrationApiKeys, WebpayPlus } = require('transbank-sdk');
 
 // Seleccionar el entorno (producción o integración) según la variable de entorno NODE_ENV
 const environment = process.env.NODE_ENV === 'production' ? Environment.Production : Environment.Integration;
 
-const transbankConfig = {
-  mall: new Options(
-    process.env.NODE_ENV === 'production' ? process.env.TRANSBANK_MALL_CODE : IntegrationCommerceCodes.WEBPAY_PLUS_MALL,
-    process.env.NODE_ENV === 'production' ? process.env.TRANSBANK_API_KEY : IntegrationApiKeys.WEBPAY,
-    environment
-  ),
-  stores: [
-    {
-      commerceCode: process.env.NODE_ENV === 'production' ? process.env.TRANSBANK_STORE_1_CODE : '597055555536',
-      name: 'Tienda 1'
-    },
-    {
-      commerceCode: process.env.NODE_ENV === 'production' ? process.env.TRANSBANK_STORE_2_CODE : '597055555537',
-      name: 'Tienda 2'
-    }
-  ]
-};
+// Configuración para Webpay Plus en una tienda individual
+const transbankConfig = new Options(
+  process.env.NODE_ENV === 'production' ? process.env.TRANSBANK_STORE_CODE : IntegrationCommerceCodes.WEBPAY_PLUS,
+  process.env.NODE_ENV === 'production' ? process.env.TRANSBANK_API_KEY : IntegrationApiKeys.WEBPAY,
+  environment
+);
 
 module.exports = { transbankConfig };
